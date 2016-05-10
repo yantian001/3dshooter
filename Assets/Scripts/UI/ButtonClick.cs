6c3979e70d99924304159659316f2bf2e8703653
yantian001@vip.qq.com <yantian001@vip.qq.com>
2016-05-10 10:28:30 +0800

    修改了代码
    1。修改了主角动画
    2。添加了通用的脚本。

---
+++ Assets/Scripts/UI/ButtonClick.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClick : MonoBehaviour
+{
+
+    public Events EventId;
+
+    public AudioClip clickClip;
+
+    Button btn;
+
+    public void OnEnable()
+    {
+        btn = GetComponent<Button>();
+        if(btn)
+        {
+            btn.onClick.AddListener(OnButtonClick);
+        }
+       
+       // StartCoroutine(CoroutineEffect());
+    }
+
+
+    //IEnumerator CoroutineEffect()
+    //{
+    //    if (effectTexture)
+    //    {
+    //        while (true)
+    //        {
+    //            yield return new WaitForSeconds(effectInterv);
+    //            var effect = Instantiate(effectTexture);
+    //            //effect.GetComponent<RectTransform>().SetParent(transform.GetComponent<RectTransform>());
+    //          //  effect.GetComponent<ButtonLight>().StartEffect();
+    //        }
+    //    }
+
+    //}
+
+
+    void OnButtonClick()
+    {
+        if(EventId != Events.NONE)
+        {
+            LeanTween.dispatchEvent((int)EventId);
+        }
+        if (clickClip)
+        {
+            LeanAudio.play(clickClip);
+        }
+      //  Debug.Log("button Clicked");
+    }
+
+    public void OnDisable()
+    {
+        if(btn)
+        {
+            btn.onClick.RemoveListener(OnButtonClick);
+        }
+    }
+}
