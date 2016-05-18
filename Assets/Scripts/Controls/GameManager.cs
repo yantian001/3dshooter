fb3a89800de8545b0d1b76b5db21673a56617e10
yantian001@vip.qq.com <yantian001@vip.qq.com>
2016-05-18 17:13:58 +0800

    修改了manager

---
+++ Assets/Scripts/Controls/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public GameStatu statu = GameStatu.Init;
+    public void OnEnable()
+    {
+        LeanTween.addListener((int)Events.TIMEUP, OnTimeUp);
+        LeanTween.addListener((int)Events.ENEMYCLEARED, OnEnemyCleared);
+    }
+
+    public void OnDisable()
+    {
+        LeanTween.removeListener((int)Events.TIMEUP, OnTimeUp);
+        LeanTween.removeListener((int)Events.ENEMYCLEARED, OnEnemyCleared);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region custom method
+    void OnTimeUp(LTEvent evt)
+    {
+
+    }
+
+    void OnEnemyCleared(LTEvent evt)
+    {
+        
+    }
+
+    /// <summary>
+    /// 更改游戏状态
+    /// </summary>
+    /// <param name="s"></param>
+    void ChangeGameStatu(GameStatu s)
+    {
+        statu = s;
+        GameValue.staus = statu;
+        
+    }
+    #endregion
+}
